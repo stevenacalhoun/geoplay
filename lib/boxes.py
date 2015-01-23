52b685e7220f23f11c356145823473714c6666c4
Steven Calhoun <stevenacalhoun@gmail.com>
2015-01-22 19:27:39 -0600

    Starting adding some library modules. Fonts and boxes are coming along nicely, more to come

---
+++ lib/boxes.py
@@ -0,0 +1,31 @@
+import pygame
+from pygame import Surface
+from pygame.locals import *
+
+class Box():
+    def __init__(self, width, height, color):
+        self._width = width
+        self._height = height
+        self._color = color
+
+    def _draw(self, screen, location=(0,0,0)):
+        pygame.draw.rect(screen, self._color, (location, (self._width, self._height)))
+
+    def drawByCenter(self, screen, center=(0,0,0)):
+        centerX, centerY = center
+
+        # pygame.draw.line(screen, (0,0,0), (0, self.locY), (1200, self.locY))
+        # pygame.draw.line(screen, (0,0,0), (self.locX, 0), (self.locX, 900))
+
+        self.locX = centerX - (self._width/2)
+        self.locY = centerY - (self._height/2)
+
+        self._draw(screen, (self.locX, self.locY))
+
+    def drawByTopLeft(self, screen, topLeft=(0,0,0)):
+        self.locX, self.locY = topLeft
+        self._draw(screen, (drawX, drawY))
+
+    def outline(self, screen, padding=(10, 10)):
+        padX, padY = padding
+        pygame.draw.rect(screen, self._color, (self.locX - (padX), self.locY - (padY), self._width + (padX*2), self._height + (padY*2)), 2)
---
+++ lib/fonts.py
@@ -0,0 +1,88 @@
+import pygame
+from pygame import Surface
+from pygame.locals import *
+
+class TextLine():
+    def __init__(self, text, font='monospace', size=16, color=(0,0,0)):
+        self.font = pygame.font.SysFont(font, size)
+        self.color_fg = color
+        self.color_bg = Color("white")
+
+        self._aa = True 
+        self._text = text
+        self.dirty = True
+
+    def _render(self):
+        # render for cache
+        """no AA = automatic transparent. With AA you need to set the color key too"""
+        self.dirty = False        
+        self.image = self.font.render(self._text, self._aa, self.color_fg)            
+        self.rect = self.image.get_rect()
+        self.width = self.rect.width
+        self.height = self.rect.height
+        self.screen = pygame.display.get_surface()
+
+    def _draw(self, screen, location=(0,0)):
+        screen.blit(self.image, location)
+
+        locX, locY = location
+
+        # pygame.draw.line(self.screen, (0,0,0), (0, locY), (1200, locY))
+        # pygame.draw.line(self.screen, (0,0,0), (locX, 0), (locX, 900))
+
+    def drawByCenter(self, screen, center=(0,0)):
+        self._render()
+        locX, locY = center
+
+        drawX = locX - (self.width/2)
+        drawY = locY - (self.height/2)
+
+        self._draw(screen, (drawX, drawY))
+
+    def drawByTopLeft(self, screen, topLeft=(0,0)):
+        self._render()
+
+        self.draw(topLeft)
+
+
+class TextBox():
+    def __init__(self, text, fontName='monospace', size=16, color=(0,0,0)):
+        self.font = pygame.font.SysFont(fontName, size)
+        self.color_fg = color
+        self.color_bg = Color("white")
+
+        self.text_lines = [ TextLine(line, font=fontName, size=size, color=color) for line in text ]        
+
+        self._aa = True 
+        self._text = text
+        self.dirty = True
+
+
+    def _render(self):
+        # render for cache
+        """no AA = automatic transparent. With AA you need to set the color key too"""
+        self.dirty = False        
+        self.image = self.font.render(self._text, self.aa, self.color_fg)            
+        self.rect = self.image.get_rect()
+        self.screen = pygame.display.get_surface()
+
+    def draw(self, screen, location=(0,0)):
+        self._render()
+        screen.blit(self.image, location)
+
+    @property
+    def text(self):
+        return self._text
+
+    @text.setter
+    def text(self, text):
+        self.dirty = True
+        self._text = text
+
+    @property
+    def aa(self): return self._aa
+
+    @aa.setter
+    def aa(self, aa):
+        self.dirty = True
+        self._aa = aa
